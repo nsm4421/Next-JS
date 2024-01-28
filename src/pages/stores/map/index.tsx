c5d30f4768ba18965e6545039bac3039873bed1e
karma <nsm4421@naver.com>
2024-01-28 20:02:27 +0900

    [FEAT] Marker Image
    서울시 공공데이터 포털에서 받아온 식당정보를 사용해, 식당에 Marker를 표시

--- src/pages/stores/map/index.tsx
+++ src/pages/stores/map/index.tsx
@@ -1,6 +1,7 @@
 /* global kakao */
 
 import Script from "next/script"
+import * as stores from "@/data/store-location-data.json"
 
 declare global {
     interface Window {
@@ -8,16 +9,33 @@ declare global {
     }
 }
 
+// gangnam station location
+const DEFAULT_LAT = 37.497624203
+const DEFAULT_LNG = 127.03088379
+
 // Reference : https://apis.map.kakao.com/web/guide/
 export default function StoreMapPage() {
     const onReady = () => {
         window.kakao.maps.load(() => {
+            // create map
             const container = document.getElementById("map")
             const option = {
-                center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+                center: new window.kakao.maps.LatLng(DEFAULT_LAT, DEFAULT_LNG),
                 level: 3
             }
-            new window.kakao.maps.Map(container, option)
+            const map = new window.kakao.maps.Map(container, option)
+
+            // marker
+            stores?.['DATA']?.map((store) => {
+                var position = new window.kakao.maps.LatLng(store?.y_dnts, store?.x_cnts);
+                var image = new window.kakao.maps.MarkerImage(
+                    `/images/marker/${store?.bizcnd_code_nm ?? 'default'}.png`,
+                    new window.kakao.maps.Size(40, 40),
+                    { offset: new window.kakao.maps.Point(25, 70) }
+                )
+                var marker = new window.kakao.maps.Marker({ position, image })
+                marker.setMap(map)
+            })
         })
     }
     const appKey = process.env.NEXT_PUBLIC_KAKAO_MAP_CLIENT
