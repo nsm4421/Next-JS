f0040f80c4ccca8253aeb811c532e583468b7c54
karma <nsm4421@naver.com>
2024-01-28 21:46:01 +0900

    [ENHENCE] Marker Image
    - 마커 선택 시 Info Box를 띄어서 상세 정보를 보여줌
    - 지도 이미지 파일을 여러 파일로 분리

---
+++ src/components/map/Markers.tsx
@@ -0,0 +1,52 @@
+import { useCallback, useEffect } from "react";
+import { Dispatch, SetStateAction } from "react"
+import { StoreType } from "@/constant/interface";
+
+interface MarkerProps {
+    map: any;
+    stores: any[],
+    setSelectedStore: Dispatch<SetStateAction<StoreType | null>>
+}
+
+export default function Markers({ map, stores, setSelectedStore }: MarkerProps) {
+
+    const loadMakers = useCallback(() => {
+        if (map) {
+            stores?.map((store) => {
+                // marker
+                var position = new window.kakao.maps.LatLng(store?.y_dnts, store?.x_cnts);
+                var image = new window.kakao.maps.MarkerImage(
+                    `/images/marker/${store?.bizcnd_code_nm ?? 'default'}.png`,
+                    new window.kakao.maps.Size(40, 40),
+                    { offset: new window.kakao.maps.Point(25, 70) }
+                )
+                var marker = new window.kakao.maps.Marker({ position, image })
+                marker.setMap(map)
+
+                // mouse hover event
+                var content = `<div class="infowindow">${store?.upso_nm}</div>`
+                var cusotmOverlay = new window.kakao.maps.CustomOverlay({
+                    position, content, xAnchor: 0.6, yAnchor: 0.9
+                })
+                window.kakao.maps.event.addListener(marker, "mouseover", () => {
+                    cusotmOverlay.setMap(map)
+
+                })
+                window.kakao.maps.event.addListener(marker, "mouseout", () => {
+                    cusotmOverlay.setMap(null)
+                })
+
+                // on click marker
+                window.kakao.maps.event.addListener(marker, "click", () => {
+                    setSelectedStore(store)
+                })
+            })
+        }
+    }, [map, setSelectedStore])
+
+    useEffect(() => {
+        loadMakers()
+    }, [loadMakers, map])
+
+    return <></>
+}
--- src/pages/stores/map/index.tsx
+++ src/pages/stores/map/index.tsx
@@ -1,7 +1,11 @@
 /* global kakao */
 
-import Script from "next/script"
-import * as stores from "@/data/store-location-data.json"
+import { useState } from "react"
+import * as storeData from "@/data/store-location-data.json"
+import Map from "@/components/map/Map"
+import Markers from "@/components/map/Markers"
+import { StoreType } from "@/constant/interface"
+import InfoBox from "@/components/map/InfoBox"
 
 declare global {
     interface Window {
@@ -9,44 +13,16 @@ declare global {
     }
 }
 
-// gangnam station location
-const DEFAULT_LAT = 37.497624203
-const DEFAULT_LNG = 127.03088379
-
-// Reference : https://apis.map.kakao.com/web/guide/
+// Reference : https://apis.map.kakao.com/web/guide/ß
 export default function StoreMapPage() {
-    const onReady = () => {
-        window.kakao.maps.load(() => {
-            // create map
-            const container = document.getElementById("map")
-            const option = {
-                center: new window.kakao.maps.LatLng(DEFAULT_LAT, DEFAULT_LNG),
-                level: 3
-            }
-            const map = new window.kakao.maps.Map(container, option)
 
-            // marker
-            stores?.['DATA']?.map((store) => {
-                var position = new window.kakao.maps.LatLng(store?.y_dnts, store?.x_cnts);
-                var image = new window.kakao.maps.MarkerImage(
-                    `/images/marker/${store?.bizcnd_code_nm ?? 'default'}.png`,
-                    new window.kakao.maps.Size(40, 40),
-                    { offset: new window.kakao.maps.Point(25, 70) }
-                )
-                var marker = new window.kakao.maps.Marker({ position, image })
-                marker.setMap(map)
-            })
-        })
-    }
-    const appKey = process.env.NEXT_PUBLIC_KAKAO_MAP_CLIENT
-
+    const stores = storeData?.['DATA']
+    const [map, setMap] = useState(null)
+    const [selectedStore, setSeletedStore] = useState<StoreType|null>(null)
 
     return <div>
-        <Script
-            strategy="afterInteractive"
-            type="text/javascript"
-            onReady={onReady}
-            src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${appKey}&autoload=false`} />
-        <div id="map" className="w-full h-screen"></div>
+        <Map setMap={setMap} />
+        <Markers map={map} stores={stores} setSelectedStore={setSeletedStore}/>
+        <InfoBox selectedStore={selectedStore} setSelectedStore={setSeletedStore}/>
     </div>
 }
